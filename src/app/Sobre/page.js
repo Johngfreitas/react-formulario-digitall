1252840334933a73e0d42ea0cb48cf2dd7dc4fe5
Jonathan Gomes De Freitas <jonathan0605@DIGITALCOLLEGE.LOCAL>
2024-06-17 21:44:09 -0300

    Primeiro projeto em Next.js

---
+++ src/app/Sobre/page.js
@@ -0,0 +1,9 @@
+export default function Sobre (){
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            Sobre
+            <a href="/" className="mt-4">
+                Ir para Home</a>
+        </main>
+    )
+}
